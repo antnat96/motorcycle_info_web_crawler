da90ac9f6540b97c3cbe5c9e95c0e7b4048c9adb
Anthony N <antnat96@gmail.com>
2020-07-07 12:47:10 -0500

    pulling data from site

--- scraper.py
+++ scraper.py
@@ -1,16 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+import xlsxwriter
 
-URL = "https://www.motorcyclespecs.co.za/"
-page = requests.get(URL)
+# Set up the workbook
+# workbook = xlsxwriter.Workbook("makes.xlsx")
+# worksheet = workbook.add_worksheet("Makes")
+row = 0
+col = 0
 
-soup = BeautifulSoup(page.content, "html.parser")
-
-makes_menu = soup.select("div[class=subMenu] > a")
-
+# Makes
+makes_URL = "https://www.motorcyclespecs.co.za/"
+makes_page = requests.get(makes_URL)
+makes_soup = BeautifulSoup(makes_page.content, "html.parser")
+makes_menu_items = makes_soup.select("div[class=subMenu] > a")
 makes_list = []
+makes_links = []
+i = 0
+j = 0
 
-for x in makes_menu:
+for x in makes_menu_items:
     makes_list.append(x.string)
+    print(makes_list[i])
+    i = i + 1
+for x in makes_menu_items:
+    makes_links.append(makes_URL + x.get("href").split("../../")[1])
+    print(makes_links[j])
+    j = j + 1
 
-print(makes_list[0])
+
+# Models
